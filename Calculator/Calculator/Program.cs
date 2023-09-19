6e9c3cdf49403a824c6b815afd488d9ad8681918
Reio4 <144784005+Reio4@users.noreply.github.com>
2023-09-19 09:14:51 +0200

    HW1

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Made by Jan Borecky for PRG seminar at Gymnazium Voderadska, year 2023-2024.
+ * Extended by students.
+ */
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            Console.WriteLine("Zadejte číslo a");
+            while (!float.TryParse(Console.ReadLine(), out a))
+                Console.WriteLine("Zadejte prosím racionální číslo");
+            Console.WriteLine("Zadejte číslo b");
+            while (!float.TryParse(Console.ReadLine(), out b))
+                Console.WriteLine("Zadejte prosím racionální číslo");
+            Console.WriteLine("Zadejte \"+\" pro součet čísel, \"-\" pro rozdíl čísel");
+
+            float result = 0;
+            string operace = Console.ReadLine();
+            while (operace != "+" && operace != "-")
+            {
+                Console.WriteLine("Zadejte prosím validní znaménko operace");
+                operace = Console.ReadLine();
+            }
+            if (operace == "+")
+                result = a + b;
+            else
+                result = a - b;
+            Console.WriteLine("Výsledek je " + result);
+
+            Console.ReadKey(); 
+        }
+    }
+}
